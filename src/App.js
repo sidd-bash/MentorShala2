1b2229011903d1a8e66f6bbadf234ec87785fbe6
sidd-bash <bhardwaj.s20@iiits.in>
2023-04-28 09:00:42 +0530

    Project Initialised

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/HomePage/Homepage'; 
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import MainPage from './components/MainPage/MainPage';
+import Admin from './components/Admin/Admin';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/mainpage" component={MainPage}/>
+          <Route exact path="/admin" component={Admin}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
